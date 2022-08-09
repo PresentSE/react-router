da58f5b3130196ef23527e806194971e38217705
Sergey <sergomail@bk.ru>
2022-08-10 00:05:26 +0300

    add navigation

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,23 @@
+import { Route, Switch } from "react-router-dom";
 import NavBar from "./components/navbar";
+import Dashboard from "./components/dashboard";
+import Login from "./components/login";
+import Posts from "./components/posts";
+import Home from "./components/home";
+import Stats from "./components/stats";
 
 function App() {
   return (
     <div>
       <NavBar />
       <h1>App</h1>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/dashboard/stats" component={Stats} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/login" component={Login} />
+        <Route path="/posts/:postId?" component={Posts} />
+      </Switch>
     </div>
   );
 }
--- src/app/components/posts.jsx
+++ src/app/components/posts.jsx
@@ -1,5 +1,19 @@
-const Home = () => {
-  return <h1>Home</h1>;
+import PostList from "./postList";
+import Post from "./post";
+
+const Posts = ({ match }) => {
+  const posts = [
+    { id: 1, label: "post 1" },
+    { id: 2, label: "post 2" },
+    { id: 3, label: "post 3" },
+  ];
+
+  const postId = match.params.postId;
+  return (
+    <>
+      {postId ? <Post posts={posts} id={postId} /> : <PostList posts={posts} />}
+    </>
+  );
 };
 
-export default Home;
+export default Posts;
